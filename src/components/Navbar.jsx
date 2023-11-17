55fd1ff17ab0da32227b90d55aa90e8f9f30f400
Parth N Mistry <parth9773021717@gmail.com>
2023-11-17 16:54:13 +0530

    add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,14 @@ const Navbar = () => {
         <p className="blue-gradient_text">PM</p>
       </NavLink>
       <nav className="flex tesxt-lg gap-7 font-medium">
-        
+        <NavLink to="/about" className={({ isActive }) => isActive ? 
+        'text-blue-500' : 'text-black'}>
+          About
+        </NavLink>
+        <NavLink to="/projects" className={({ isActive }) => isActive ? 
+        'text-blue-500' : 'text-black'}>
+          Projetcs
+        </NavLink>
       </nav>
     </header>
   )
