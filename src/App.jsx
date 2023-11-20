7d5a51ba049f900662ee39bf245e209d47f65a2a
Parth N Mistry <parth9773021717@gmail.com>
2023-11-20 19:51:54 +0530

    add 3d assistes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Route, BrowserRouter as Router, Routes} from 
 'react-router-dom';
+
+import { Home, About, Projects, Contact } from './pages';
 import Navbar from './components/Navbar';
 
 const App = () => {
